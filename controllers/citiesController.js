ab46bc44c26b1705481e82f777194c09e665d45f
letovaasd <127133120+letovaasd@users.noreply.github.com>
2023-05-26 17:32:35 +0300

    test

--- controllers/citiesController.js
+++ controllers/citiesController.js
@@ -1,3 +1,4 @@
+const { BOOLEAN } = require("sequelize/types");
 const ApiError = require("../error/ApiError");
 const { Cities } = require("../models/models");
 
@@ -61,7 +62,7 @@ function getObjectForDataBase(item) {
 }
 
 function checkValue(check_base, check_speaker, check_scenario) {
-  switch (BOOLEAN) {
+  switch (Boolean) {
     case typeof check_base:
       return { check_base: !!check_base };
     case typeof check_speaker:
@@ -275,7 +276,7 @@ class CitiesController {
     if (!id && !id_for_base) {
       return next(ApiError.badRequest("Укажите id или id_for_base"));
     }
-    if (typeof (check_base ?? check_speaker ?? check_scenario) !== BOOLEAN) {
+    if (typeof (check_base ?? check_speaker ?? check_scenario) !== Boolean) {
       return next(ApiError.badRequest("Укажите данные для замены"));
     }
     const city = (await Cities.findOne({ where: { id: Number(id) || null } })) || (await Cities.findOne({ where: { id_for_base: item.id_for_base } }));
