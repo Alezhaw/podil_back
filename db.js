96c264deed71ba67a16910353a30d1cce460ef92
xqfech <31478361+xqfech@users.noreply.github.com>
2023-05-30 12:05:47 +0300

    fixed local connect to db

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME, // Название БД
-    process.env.DB_USER, // Пользователь
-    process.env.DB_PASSWORD, // ПАРОЛЬ
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+module.exports = new Sequelize(process.env.DB_LINK, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true,
+      native:true
     }
-)
+  });
