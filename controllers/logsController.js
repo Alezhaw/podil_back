34a9cddfa8e1731d9b76282ea21f4cc368cb6be0
letovaasd <127133120+letovaasd@users.noreply.github.com>
2023-06-14 14:54:54 +0300

    add search logs

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -7,9 +7,47 @@ class LogsController {
     return res.json(logs);
   }
 
+  async getFilteredLogsCities(req, res, next) {
+    const { search, country, updateFilter, createFilter, deleteFilter, pageSize, page } = req.body;
+
+    if (!pageSize || !page) {
+      return next(ApiError.badRequest("Укажите page и pageSize"));
+    }
+    const city = await Logs.findAll();
+    if (!city) {
+      return next(ApiError.internal("Нет логов по городам"));
+    }
+
+    let filteredCities = city
+      ?.filter((el) => (search ? el?.miasto_lokal?.toLowerCase()?.includes(search.toLowerCase()) : true))
+      ?.filter((checkbox) => (checkbox?.action === "update" && updateFilter) || (checkbox?.action === "create" && createFilter) || (checkbox?.action === "delete" && deleteFilter))
+      ?.filter((el) => (country ? country === el.country : true))
+      ?.slice(page * pageSize - pageSize, page * pageSize);
+    return res.json(filteredCities);
+  }
+
   async getAllBasesLog(req, res) {
     const logs = await LogsForBase.findAll();
     return res.json(logs);
+  }
+
+  async getFilteredLogsBases(req, res, next) {
+    const { search, country, updateFilter, createFilter, deleteFilter, pageSize, page } = req.body;
+
+    if (!pageSize || !page) {
+      return next(ApiError.badRequest("Укажите page и pageSize"));
+    }
+    const base = await LogsForBase.findAll();
+    if (!base) {
+      return next(ApiError.internal("Нет логов по городам"));
+    }
+
+    let filteredBases = base
+      ?.filter((el) => (search ? el?.base_id?.toLowerCase()?.includes(search.toLowerCase()) : true))
+      ?.filter((checkbox) => (checkbox?.action === "update" && updateFilter) || (checkbox?.action === "create" && createFilter) || (checkbox?.action === "delete" && deleteFilter))
+      ?.filter((el) => (country ? country === el.country : true))
+      ?.slice(page * pageSize - pageSize, page * pageSize);
+    return res.json(filteredBases);
   }
 }
 
--- routes/logsRouter.js
+++ routes/logsRouter.js
@@ -4,6 +4,8 @@ const auth = require("../middleware/authMiddleware");
 const logsController = require("../controllers/logsController");
 
 router.get("/getCities", auth, logsController.getAllCitiesLog);
+router.post("/searchCity", auth, logsController.getFilteredLogsCities);
 router.get("/getBases", auth, logsController.getAllBasesLog);
+router.post("/searchCity", auth, logsController.getFilteredLogsBases);
 
 module.exports = router;
