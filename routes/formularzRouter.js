23c73dc8eab4ae0293d651198455328f878f3ecb
letovaasd <127133120+letovaasd@users.noreply.github.com>
2023-07-24 16:09:14 +0300

    add create to router

--- routes/formularzRouter.js
+++ routes/formularzRouter.js
@@ -3,13 +3,7 @@ const router = new Router();
 const auth = require("../middleware/authMiddleware");
 const formularzController = require("../controllers/formularzController");
 
-//router.post("/create", auth, citiesController.create);
-//router.post("/getOne", auth, citiesController.getOneCity);
-//router.post("/search", auth, citiesController.getFilteredCities);
-//router.post("/changeCheck", auth, citiesController.changeCheck);
-//router.post("/changeStatus", auth, citiesController.changeStatus);
-//router.post("/deleteOne", auth, citiesController.deleteCity);
-//router.post("/deleteTime", auth, citiesController.deleteOneTime);
+router.post("/create", auth, formularzController.create);
 router.get("/getAll", auth, formularzController.getAll);
 router.get("/getActive", auth, formularzController.getActiveApplications);
 router.get("/getInvalid", auth, formularzController.getInvalidApplications);
