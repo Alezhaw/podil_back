ca00257e320574d1b7e467d73ad15f2a1ebd89a7
letovaasd <127133120+letovaasd@users.noreply.github.com>
2023-07-26 02:57:07 +0300

    test

--- routes/formularzRouter.js
+++ routes/formularzRouter.js
@@ -3,10 +3,10 @@ const router = new Router();
 const auth = require("../middleware/authMiddleware");
 const formularzController = require("../controllers/formularzController");
 
-router.post("/create", auth, formularzController.create);
-router.get("/getAll", auth, formularzController.getAll);
-router.get("/getActive", auth, formularzController.getActiveApplications);
-router.get("/getInvalid", auth, formularzController.getInvalidApplications);
-router.get("/newScript", auth, formularzController.updateNewScriptParametr);
+router.post("/create", formularzController.create);
+router.get("/getAll", formularzController.getAll);
+router.get("/getActive", formularzController.getActiveApplications);
+router.get("/getInvalid", formularzController.getInvalidApplications);
+router.get("/newScript", formularzController.updateNewScriptParametr);
 
 module.exports = router;
