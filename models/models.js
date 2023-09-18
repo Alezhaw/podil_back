4422e947e8391858307ff109a4f9c038cd216921
AntonSamko <anton.samko@nure.ua>
2023-09-18 21:16:29 +0300

    add

--- models/models.js
+++ models/models.js
@@ -6,6 +6,7 @@ const SpeakerTemplate = sequelize.define("speaker_template", {
   text: { type: DataTypes.STRING },
   type: { type: DataTypes.INTEGER },
   name: { type: DataTypes.STRING, unique: true },
+  country: { type: DataTypes.STRING },
 });
 const Logs = sequelize.define("log", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
@@ -81,6 +82,64 @@ const PlBases = sequelize.define("plbase", {
   sogl_3: { type: DataTypes.INTEGER },
   sogl_4: { type: DataTypes.INTEGER },
   comment: { type: DataTypes.STRING },
+});
+
+const ContactStatus = sequelize.define("contract_status", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const PlContactStatus = sequelize.define("plcontract_status", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const KzContactStatus = sequelize.define("kzcontract_status", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const ReservationStatus = sequelize.define("reservation_status", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const PlReservationStatus = sequelize.define("plreservation_status", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const KzReservationStatus = sequelize.define("kzreservation_status", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const Regiment = sequelize.define("regiment", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const PlRegiment = sequelize.define("plregiment", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const KzRegiment = sequelize.define("kzregiment", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+
+const ProjectSales = sequelize.define("project_sales", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+const PlProjectSales = sequelize.define("plproject_sales", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+});
+const KzProjectSales = sequelize.define("kzproject_sales", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
 });
 
 const Cities = sequelize.define("city", {
@@ -331,4 +390,16 @@ module.exports = {
   Logs,
   LogsForBase,
   SpeakerTemplate,
+  ReservationStatus,
+  PlReservationStatus,
+  KzReservationStatus,
+  Regiment,
+  PlRegiment,
+  KzRegiment,
+  ProjectSales,
+  PlProjectSales,
+  KzProjectSales,
+  ContactStatus,
+  PlContactStatus,
+  KzContactStatus,
 };
