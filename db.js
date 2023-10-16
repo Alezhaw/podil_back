12fe4ca85acecc4727d48f1ef3a3e14769f64a6c
letovaasd <127133120+letovaasd@users.noreply.github.com>
2023-10-16 20:30:25 +0300

    add docker files, change db

--- db.js
+++ db.js
@@ -3,8 +3,10 @@ const { Sequelize } = require("sequelize");
 module.exports = new Sequelize(process.env.DB_LINK, {
   dialect: "postgres",
   protocol: "postgres",
+  port: process.env.DB_PORT,
+  host: "db",
   dialectOptions: {
-    ssl: true,
+    // ssl: true,
     native: true,
   },
 });
