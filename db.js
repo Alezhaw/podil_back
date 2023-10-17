2d7f47146549a8b50b3e422a632088da268c5b76
letovaasd <127133120+letovaasd@users.noreply.github.com>
2023-10-17 05:03:26 +0300

    fix

--- db.js
+++ db.js
@@ -14,7 +14,6 @@ module.exports = new Sequelize(process.env.DB_LINK, {
   dialect: "postgres",
   protocol: "postgres",
   port: process.env.DB_PORT,
-  host: "db",
   dialectOptions: {
     ssl: true,
     native: true,
