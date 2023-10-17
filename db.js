19931095cec8e5606b9bec454ea180dfaa47ec8f
letovaasd <127133120+letovaasd@users.noreply.github.com>
2023-10-17 05:00:25 +0300

    fix

--- db.js
+++ db.js
@@ -1,12 +1,22 @@
 const { Sequelize } = require("sequelize");
 
+// module.exports = new Sequelize(process.env.DB_LINK, {
+//   dialect: "postgres",
+//   protocol: "postgres",
+//   port: process.env.DB_PORT,
+//   host: "db",
+//   dialectOptions: {
+//     native: true,
+//   },
+// }); for docker
+
 module.exports = new Sequelize(process.env.DB_LINK, {
   dialect: "postgres",
   protocol: "postgres",
   port: process.env.DB_PORT,
   host: "db",
   dialectOptions: {
-    // ssl: true,
+    ssl: true,
     native: true,
   },
 });
